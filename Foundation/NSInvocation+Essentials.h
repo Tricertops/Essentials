9ab7e701dd7a061472412a8340cd2c422edce31c
Martin Kiss <martin.kiss@me.com>
2013-09-24 00:50:58 +0200

    + NSinvocation methods for accessing arguments and return value

--- Foundation/NSInvocation+Essentials.h
+++ Foundation/NSInvocation+Essentials.h
@@ -16,8 +16,42 @@
 
 
 
+#pragma mark Creating
+
 /// Quick way of creating invocations. Arguments are used to obtain method signature and then are assigned to invocation.
 + (instancetype)invocationWithTarget:(id)target selector:(SEL)sel;
+
+
+
+#pragma mark Arguments
+
+/// Retrieves object argument at given index. Index should ignore `self` and `_cmd`, so the first argument after those two is at index 0.
+- (id)objectArgumentAtIndex:(NSInteger)index;
+
+/// Sets object argument at given index. Index should ignore `self` and `_cmd`, so the first argument after those two is at index 0.
+- (void)setObjectArgument:(id)object atIndex:(NSInteger)index;
+
+/// Retrieves integer argument at given index. Index should ignore `self` and `_cmd`, so the first argument after those two is at index 0.
+- (NSInteger)integerArgumentAtIndex:(NSInteger)index;
+
+/// Sets integer argument at given index. Index should ignore `self` and `_cmd`, so the first argument after those two is at index 0.
+- (void)setIntegerArgument:(NSInteger)integer atIndex:(NSInteger)index;
+
+
+
+#pragma mark Return Value
+
+/// Retrieves return value as object.
+- (id)objectReturnValue;
+
+/// Sets return value as object.
+- (void)setObjectReturnValue:(id)object;
+
+/// Retrieves return value as integer.
+- (NSInteger)integerReturnValue;
+
+/// Sets return value as integer.
+- (void)setIntegerReturnValue:(NSInteger)integer;
 
 
 
--- Foundation/NSInvocation+Essentials.m
+++ Foundation/NSInvocation+Essentials.m
@@ -7,6 +7,7 @@
 //
 
 #import "NSInvocation+Essentials.h"
+#import "Foundation+Essentials.h"
 
 
 
@@ -27,7 +28,84 @@
 }
 
 
+- (id)objectArgumentAtIndex:(NSInteger)index {
+    index += 2; // move beyond `self` and `_cmd`
+    __unsafe_unretained id argument = nil;
+    const char *argumentType = [self.methodSignature getArgumentTypeAtIndex:index];
+    ESSAssert(strcmp(argumentType, @encode(id)) == 0,
+              @"Incorrect argument type '%s' of method '-%@'", argumentType, NSStringFromSelector(self.selector)) return nil;
+    [self getArgument:&argument atIndex:index];
+    id arcArgument = argument;
+    return arcArgument;
+}
 
+
+- (void)setObjectArgument:(id)arcObject atIndex:(NSInteger)index {
+    index += 2; // move beyond `self` and `_cmd`
+    __unsafe_unretained id object = arcObject;
+    const char *argumentType = [self.methodSignature getArgumentTypeAtIndex:index];
+    ESSAssert(strcmp(argumentType, @encode(id)) == 0,
+              @"Incorrect argument type '%s' of method '-%@'", argumentType, NSStringFromSelector(self.selector)) return;
+    [self setArgument:&object atIndex:index];
+}
+
+
+- (NSInteger)integerArgumentAtIndex:(NSInteger)index {
+    index += 2; // move beyond `self` and `_cmd`
+    NSInteger integer;
+    const char *argumentType = [self.methodSignature getArgumentTypeAtIndex:index];
+    ESSAssert(strcmp(argumentType, @encode(NSInteger)) == 0,
+              @"Incorrect argument type '%s' in method '-%@'", argumentType, NSStringFromSelector(self.selector)) return NSNotFound;
+    [self getArgument:&integer atIndex:index];
+    return integer;
+}
+
+
+- (void)setIntegerArgument:(NSInteger)integer atIndex:(NSInteger)index {
+    index += 2; // move beyond `self` and `_cmd`
+    const char *argumentType = [self.methodSignature getArgumentTypeAtIndex:index];
+    ESSAssert(strcmp(argumentType, @encode(NSInteger)) == 0,
+              @"Incorrect argument type '%s' in method '-%@'", argumentType, NSStringFromSelector(self.selector)) return;
+    [self setArgument:&integer atIndex:index];
+}
+
+
+- (id)objectReturnValue {
+    __unsafe_unretained id returnValue = nil;
+    const char *returnType = [self.methodSignature methodReturnType];
+    ESSAssert(strcmp(returnType, @encode(id)) == 0,
+              @"Incorrect return type '%s' of method '-%@'", returnType, NSStringFromSelector(self.selector)) return nil;
+    [self getReturnValue:&returnValue];
+    id arcReturnValue = returnValue;
+    return arcReturnValue;
+}
+
+
+- (void)setObjectReturnValue:(id)arcObject {
+    __unsafe_unretained id object = arcObject;
+    const char *returnType = [self.methodSignature methodReturnType];
+    ESSAssert(strcmp(returnType, @encode(id)) == 0,
+              @"Incorrect return type '%s' of method '-%@'", returnType, NSStringFromSelector(self.selector)) return;
+    [self setReturnValue:&object];
+}
+
+
+- (NSInteger)integerReturnValue {
+    NSInteger returnValue;
+    const char *returnType = [self.methodSignature methodReturnType];
+    ESSAssert(strcmp(returnType, @encode(NSInteger)) == 0,
+              @"Incorrect return type '%s' of method '-%@'", returnType, NSStringFromSelector(self.selector)) return NSNotFound;
+    [self getReturnValue:&returnValue];
+    return returnValue;
+}
+
+
+- (void)setIntegerReturnValue:(NSInteger)integer {
+    const char *returnType = [self.methodSignature methodReturnType];
+    ESSAssert(strcmp(returnType, @encode(NSInteger)) == 0,
+              @"Incorrect return type '%s' of method '-%@'", returnType, NSStringFromSelector(self.selector)) return;
+    [self setReturnValue:&integer];
+}
 
 
 @end
