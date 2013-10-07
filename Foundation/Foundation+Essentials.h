8e139a5ff7548d3e28131175490d7e25ca7ce383
Martin Kiss <martin.kiss@me.com>
2013-10-07 20:11:57 +0200

    Comments for Foundation

--- Foundation/Foundation+Essentials.h
+++ Foundation/Foundation+Essentials.h
@@ -20,27 +20,39 @@
 
 
 
+
+
 #pragma mark - Assertions
 
 /// WARNING: Any code after ESSAssert macro is conditioned by the assertion condition! If you forget to put trailing semicolon, results are unpredictable.
 
 
+
 #if !defined(NS_BLOCK_ASSERTIONS)
 
+
+/// Use like NSAssert, but you can append code to be executed on Release.
 #define ESSAssert(CONDITION, MESSAGE, ...)\
 if ( ! (CONDITION) && (( [[NSAssertionHandler currentHandler] handleFailureInFunction:[NSString stringWithUTF8String:__PRETTY_FUNCTION__] file:[NSString stringWithUTF8String:__FILE__] lineNumber:__LINE__ description:MESSAGE, ##__VA_ARGS__], YES)) )
 
 #else
 
+
+/// Use like NSAssert, but you can append code to be executed on Release.
 #define ESSAssert(CONDITION, MESSAGE, ...)\
 if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition not satisfied: %s, reason: '" MESSAGE "'", __PRETTY_FUNCTION__, __FILE__, __LINE__, #CONDITION, ##__VA_ARGS__), YES)) )
 
 #endif
 
-#define ESSAssertReturn(CONDITION, MESSAGE, ...)        ESSAssert(CONDITION, MESSAGE, ##__VA_ARGS__) return
-#define ESSAssertReturnNil(CONDITION, MESSAGE, ...)     ESSAssert(CONDITION, MESSAGE, ##__VA_ARGS__) return nil
+
+/// Use like NSAssert, but this one will throw exception even on Release.
 #define ESSAssertException(CONDITION, MESSAGE, ...)     ESSAssert(CONDITION, MESSAGE, ##__VA_ARGS__) @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:[NSString stringWithFormat:@"*** Assertion failure in %s, %s:%d, Condition not satisfied: %s, reason: '" MESSAGE "'", __PRETTY_FUNCTION__, __FILE__, __LINE__, #CONDITION, ##__VA_ARGS__] userInfo:nil]
+
+
+/// Use like NSAssert(NO, ...), append code to be executed on Release.
 #define ESSAssertFail(MESSAGE, ...)                     ESSAssert(NO, MESSAGE, ##__VA_ARGS__)
+
+
 
 
 
@@ -49,6 +61,7 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 #pragma mark - Shared Class Values
 
 
+/// Use to create class method that uses dispatch_once with simple value.
 #define ESSShared(TYPE, NAME, VALUE)\
 + (TYPE)NAME {\
     static TYPE NAME = nil;\
@@ -60,6 +73,7 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 }\
 
 
+/// Use to create class method that uses dispatch_once with additional code. Append method body just after the macro and return object to be shared.
 #define ESSSharedMake(TYPE, NAME)\
 + (TYPE)NAME {\
     static TYPE NAME = nil;\
@@ -75,14 +89,18 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 
 
 
+
+
 #pragma mark Properties
 
 
+/// Use if you are lazy to declare read-only lazy-loaded property. Will declare selector, that forces you to use ESSLazyLoad too.
 #define ESSPropertyLazy(MEMORY, TYPE, NAME)\
 @property (nonatomic, readonly, MEMORY) TYPE NAME;\
 - (TYPE)ess_make_##NAME;
 
 
+/// Use to create lazy-loading getter. Append implementation that assigns desired value into the property.
 #define ESSLazyLoad(TYPE, GETTER)\
 - (TYPE)GETTER {\
     if ( ! self->_##GETTER) {\
@@ -93,6 +111,7 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 - (void)load_##GETTER\
 
 
+/// Use to create lazy-loading getter. Append implementation that returns desired value.
 #define ESSLazyMake(TYPE, GETTER)\
 @synthesize GETTER = _##GETTER;\
 - (TYPE)GETTER {\
@@ -104,7 +123,7 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 - (TYPE)ess_make_##GETTER\
 
 
-
+/// Use to create accessors for associated property.
 #define ESSSynthesizeStrong(TYPE, GETTER, SETTER)\
 - (TYPE)GETTER {\
     return [self associatedObjectForKey:@selector(GETTER)];\
@@ -114,6 +133,7 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 }\
 
 
+/// Use to create accessors for associated property. Append implementation that assigns desired value into the property using setter.
 #define ESSSynthesizeStrongLoad(TYPE, GETTER, SETTER)\
 - (TYPE)GETTER {\
     TYPE GETTER = [self associatedObjectForKey:@selector(GETTER)];\
@@ -129,6 +149,7 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 - (void)load_##GETTER\
 
 
+/// Use to create accessors for associated property. Append implementation that returns desired value.
 #define ESSSynthesizeStrongMake(TYPE, GETTER, SETTER)\
 - (TYPE)GETTER {\
     TYPE GETTER = [self associatedObjectForKey:@selector(GETTER)];\
@@ -144,6 +165,7 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 - (TYPE)make_##GETTER\
 
 
+/// Use to create accessors for associated NSValue property. Specify primitive TYPE to be wrapped using NSValue.
 #define ESSSynthesizeValue(TYPE, GETTER, SETTER)\
 - (TYPE)GETTER {\
     TYPE GETTER;\
@@ -160,14 +182,18 @@ if ( ! (CONDITION) && (( NSLog(@"*** Assertion failure in %s, %s:%d, Condition n
 
 
 
+
+
 #pragma mark String Definitions
 
 
-#define ESSStringDeclaration(PREFIX, TYPE, NAME)\
-extern NSString * const   PREFIX ## TYPE ## NAME;
+/// Use to declare 3-part string constatnt.
+#define ESSStringDeclaration(PREFIX, TYPE, NAME)        extern NSString * const   PREFIX ## TYPE ## NAME;
 
-#define ESSStringDefinition(PREFIX, TYPE, NAME)\
-NSString * const   PREFIX ## TYPE ## NAME   =   @ #PREFIX "." #TYPE "." #NAME;
+/// Use to define 3 part string constant. The value is joined using dots.
+#define ESSStringDefinition(PREFIX, TYPE, NAME)         NSString * const   PREFIX ## TYPE ## NAME   =   @ #PREFIX "." #TYPE "." #NAME;
+
+
 
 
 
@@ -176,8 +202,11 @@ NSString * const   PREFIX ## TYPE ## NAME   =   @ #PREFIX "." #TYPE "." #NAME;
 #pragma mark Key Path Operators
 
 
+/// Don't use directly, use one of the concrete macros below.
 #define keypathOperation(OPERATION, CLASS, KEYPATH)    (((void)(NO && ((void)CLASS.new.KEYPATH, NO)), "@" # OPERATION "." # KEYPATH))
 
+
+/// Use to create NSString from keypath to be used as keypath operator. Example:   @keypathAverage(NSString, length)
 #define keypathCount(CLASS, KEYPATH)    keypathOperation(count, CLASS, KEYPATH)
 #define keypathSum(CLASS, KEYPATH)      keypathOperation(sum, CLASS, KEYPATH)
 #define keypathAverage(CLASS, KEYPATH)  keypathOperation(avg, CLASS, KEYPATH)
@@ -186,27 +215,42 @@ NSString * const   PREFIX ## TYPE ## NAME   =   @ #PREFIX "." #TYPE "." #NAME;
 #define keypathUnique(CLASS, KEYPATH)   keypathOperation(distinctUnionOfObjects, CLASS, KEYPATH)
 
 
-
-
+/// Use to create string from selector. Some validation and completion included.
 #define ESSSelector(SELECTOR)  NSStringFromSelector(@selector(SELECTOR))
 
+/// Use to create string from selector. Full validation and completion included.
+#define ESSKeypath(OBJECT, KEYPATH)         (((void)(NO && ((void)OBJECT.KEYPATH, NO)), # KEYPATH))
+
+/// Use to create string from selector if you don't have an instance by hand. Full validation and completion included.
+#define ESSKeypathClass(CLASS, KEYPATH)     ESSKeypath(CLASS.new, KEYPATH)
 
 
+/// Unusable in current version of Clang.
 #define ESSDeprecated      __attribute__((deprecated(message)))
 #define ESSDeprecatedUse(selector)  ESSDeprecated("Use " # selector " instead")
 
 
 
-#pragma mark - Random
-
-
-extern NSUInteger NSUIntegerRandom(NSUInteger count);
 
 
 
 
-#pragma mark - Clamping
+#pragma mark - Numbers
 
+
+/// Unsigned byte.
+typedef uint8_t NSUByte;
+
+
+/// Signed byte.
+typedef int8_t NSByte;
+
+
+/// Uses arc4random_uniform(). Pass NSUIntegerMax to use arc4random().
+extern inline NSUInteger NSUIntegerRandom(NSUInteger count);
+
+
+/// Use to make sure the VALUE is no less than MIN and no more than MAX.
 #define CLAMP(MIN, VALUE, MAX) ({\
     typeof(MIN) __min = (MIN);\
     typeof(VALUE) __value = (VALUE);\
@@ -215,20 +259,22 @@ extern NSUInteger NSUIntegerRandom(NSUInteger count);
 })
 
 
-
-
-#pragma mark - Types
-
-
-typedef uint8_t NSUByte;
-typedef unsigned char NSByte;
-
+/// Should be infinity, uses HUGE_VAL.
 extern NSTimeInterval const NSTimeIntervalInfinity;
 
 
 
-#pragma mark - Equality
 
+
+
+
+#pragma mark - Objects
+
+/// Safely compares two objects using `==` and `-isEqual:` to really tell is those two are equal. Exception-free.
 extern inline BOOL NSEqual(NSObject *, NSObject *);
+
+
+
+
 
 
