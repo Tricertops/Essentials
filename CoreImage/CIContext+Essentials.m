52fa2606f8fa5352108a1dd9333a385a9866dc9f
Martin Kiss <martin.kiss@me.com>
2016-03-20 09:52:46 +0100

    CIContext handles infinite extent of some filters

--- CoreImage/CIContext+Essentials.m
+++ CoreImage/CIContext+Essentials.m
@@ -55,7 +55,13 @@
     [filters.firstObject setValue:inputCI forKey:kCIInputImageKey];
     CIFilter *finalFilter = [CIFilter chainFilters:filters];
     
-    CGImageRef outputCG = [self createCGImage:finalFilter.outputImage fromRect:inputCI.extent];
+    CIImage *outputCI = finalFilter.outputImage;
+    CGRect extent = outputCI.extent;
+    if (CGRectIsInfinite(extent)) {
+        NSLog(@"%s: Output image is infinite, crop it.", __PRETTY_FUNCTION__);
+        extent = inputCI.extent;
+    }
+    CGImageRef outputCG = [self createCGImage:outputCI fromRect:extent];
     UIImage *outputUI = [UIImage imageWithCGImage:outputCG scale:inputUI.scale orientation:inputUI.imageOrientation];
     CGImageRelease(outputCG);
     
