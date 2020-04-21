7be0255c86f5e8da443d06e8819eafb2a55ef9b7
Martin Kiss <martin.kiss@me.com>
2020-04-21 10:30:58 +0200

    Update UIDevice with new hardware identifiers

--- UIKit/UIDevice+Essentials.m
+++ UIKit/UIDevice+Essentials.m
@@ -250,6 +250,10 @@
                   @"iPhone11,6": @"iPhone XS Max", //! 2018
                   @"iPhone11,8": @"iPhone XR", //! 2018
                   
+                  @"iPhone12,1": @"iPhone 11", //! 2019
+                  @"iPhone12,3": @"iPhone 11 Pro", //! 2019
+                  @"iPhone12,5": @"iPhone 11 Pro Max", //! 2019
+                  
                   //! iPod Touch
                   @"iPod1,1": @"iPod Touch 1", //! 2007
                   @"iPod2,1": @"iPod Touch 2", //! 2008
@@ -257,54 +261,67 @@
                   @"iPod4,1": @"iPod Touch 4", //! 2010
                   @"iPod5,1": @"iPod Touch 5", //! 2012
                   @"iPod7,1": @"iPod Touch 6", //! 2015
+                  @"iPod9,1": @"iPod Touch 7", //! 2019
                   
                   //! iPad
                   @"iPad1,1": @"iPad 1", //! 2010
                   @"iPad1,2": @"iPad 1", //! Cellular, 2010
-                  
                   @"iPad2,1": @"iPad 2", //! Wi-Fi, 2011
                   @"iPad2,2": @"iPad 2", //! GSM, 2011
                   @"iPad2,3": @"iPad 2", //! CDMA, 2011
                   @"iPad2,4": @"iPad 2", //! 2012
-                  @"iPad2,5": @"iPad Mini 1", //! Wi-Fi, 2012
-                  @"iPad2,6": @"iPad Mini 1", //! GSM, 2012
-                  @"iPad2,7": @"iPad Mini 1", //! Global, 2012
-                  
                   @"iPad3,1": @"iPad 3", //! Wi-Fi, 2012
                   @"iPad3,2": @"iPad 3", //! CDMA, 2012
                   @"iPad3,3": @"iPad 3", //! GSM, 2012
                   @"iPad3,4": @"iPad 4", //! Wi-Fi, 2012
                   @"iPad3,5": @"iPad 4", //! GSM, 2012
                   @"iPad3,6": @"iPad 4", //! Global, 2012
+                  @"iPad6,11": @"iPad 5", //! Wi-Fi, 2017
+                  @"iPad6,12": @"iPad 5", //! Cellular, 2017
+                  @"iPad7,5": @"iPad 6", //! Wi-Fi, 2018
+                  @"iPad7,6": @"iPad 6", //! Cellular, 2018
+                  @"iPad7,11": @"iPad 7", //! Wi-Fi, 2019
+                  @"iPad7,12": @"iPad 7", //! Cellular, 2019
                   
-                  @"iPad4,1": @"iPad Air 1", //! Wi-Fi, 2013
-                  @"iPad4,2": @"iPad Air 1", //! Cellular, 2013
-                  @"iPad4,3": @"iPad Air 1", //! China, 2013
+                  @"iPad2,5": @"iPad Mini 1", //! Wi-Fi, 2012
+                  @"iPad2,6": @"iPad Mini 1", //! GSM, 2012
+                  @"iPad2,7": @"iPad Mini 1", //! Global, 2012
                   @"iPad4,4": @"iPad Mini 2", //! Wi-Fi, 2013
                   @"iPad4,5": @"iPad Mini 2", //! Cellular, 2013
                   @"iPad4,6": @"iPad Mini 2", //! China, 2013
                   @"iPad4,7": @"iPad Mini 3", //! Wi-Fi, 2014
                   @"iPad4,8": @"iPad Mini 3", //! Cellular, 2014
-                  
                   @"iPad5,1": @"iPad Mini 4", //! Wi-Fi, 2015
                   @"iPad5,2": @"iPad Mini 4", //! Cellular, 2015
+                  
+                  @"iPad4,1": @"iPad Air 1", //! Wi-Fi, 2013
+                  @"iPad4,2": @"iPad Air 1", //! Cellular, 2013
+                  @"iPad4,3": @"iPad Air 1", //! China, 2013
                   @"iPad5,3": @"iPad Air 2", //! Wi-Fi, 2014
                   @"iPad5,4": @"iPad Air 2", //! Cellular, 2014
+                  @"iPad11,3": @"iPad Air 3", //! Wi-Fi, 2019
+                  @"iPad11,4": @"iPad Air 3", //! Cellular, 2019
                   
-                  @"iPad6,7": @"iPad Pro 12.9″ 1", //! Wi-Fi, 2015
-                  @"iPad6,8": @"iPad Pro 12.9″ 1", //! Cellular, 2015
-                  
-                  @"iPad6,3": @"iPad Pro 9.7″ 1", //! Wi-Fi, 2016
-                  @"iPad6,4": @"iPad Pro 9.7″ 1", //! Cellular, 2016
-                  
-                  @"iPad6,11": @"iPad 5", //! Wi-Fi, 2017
-                  @"iPad6,12": @"iPad 5", //! Cellular, 2017
-                  
-                  @"iPad7,1": @"iPad Pro 12.9″ 2", //! Wi-Fi, 2017
-                  @"iPad7,2": @"iPad Pro 12.9″ 2", //! Cellular, 2017
-                  
-                  @"iPad7,3": @"iPad Pro 10.5″ 1", //! Wi-Fi, 2017
-                  @"iPad7,4": @"iPad Pro 10.5″ 1", //! Cellular, 2017
+                  @"iPad6,7": @"iPad Pro 1", //! Wi-Fi, 12.9-inch, 2015
+                  @"iPad6,8": @"iPad Pro 1", //! Cellular, 12.9-inch, 2015
+                  @"iPad6,3": @"iPad Pro 1", //! Wi-Fi, 9.7-inch, 2016
+                  @"iPad6,4": @"iPad Pro 1", //! Cellular, 9.7-inch, 2016
+                  @"iPad7,1": @"iPad Pro 2", //! Wi-Fi, 12.9-inch, 2017
+                  @"iPad7,2": @"iPad Pro 2", //! Cellular, 12.9-inch, 2017
+                  @"iPad7,3": @"iPad Pro 2", //! Wi-Fi, 10.5-inch, 2017
+                  @"iPad7,4": @"iPad Pro 2", //! Cellular, 10.5-inch, 2017
+                  @"iPad8,1": @"iPad Pro 3", //! Wi-Fi, 11-inch, 4 GB, 2018
+                  @"iPad8,2": @"iPad Pro 3", //! Wi-Fi, 11-inch, 6 GB, 2018
+                  @"iPad8,3": @"iPad Pro 3", //! Cellular, 11-inch, 4 GB, 2018
+                  @"iPad8,4": @"iPad Pro 3", //! Cellular, 11-inch, 6 GB, 2018
+                  @"iPad8,5": @"iPad Pro 3", //! Wi-Fi, 12.9-inch, 4 GB, 2018
+                  @"iPad8,6": @"iPad Pro 3", //! Wi-Fi, 12.9-inch, 6 GB, 2018
+                  @"iPad8,7": @"iPad Pro 3", //! Cellular, 12.9-inch, 4 GB, 2018
+                  @"iPad8,8": @"iPad Pro 3", //! Cellular, 12.9-inch, 6 GB, 2018
+                  @"iPad8,9": @"iPad Pro 4", //! Wi-Fi, 11-inch, 2020
+                  @"iPad8,10": @"iPad Pro 4", //! Cellular, 11-inch, 2020
+                  @"iPad8,11": @"iPad Pro 4", //! Wi-Fi, 12.9-inch, 2020
+                  @"iPad8,12": @"iPad Pro 4", //! Cellular, 12.9-inch, 2020
                   
                   //! Apple TV
                   @"AppleTV1,1": @"Apple TV 1", //! macOS, 2007
