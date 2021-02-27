5277bd0f4e39eb8f2e4cac123cc0d6714bac9974
Martin Kiss <martin.kiss@me.com>
2021-02-27 09:03:07 +0100

    Add some Swift so it’s fully compatible with Swift

---
+++ Essentials.swift
@@ -0,0 +1,9 @@
+//
+//  Essentials.swift
+//  Essentials
+//
+//  Created by Martin Kiss on 27 Feb 2021.
+//  Copyright © 2021 Tricertops. All rights reserved.
+//
+
+/// This file needs to be in Essentials framework for it be compatible with Swift in the app.
